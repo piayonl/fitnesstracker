89fe04eba215ae1c7d79f0d7588bd8975bf5eddb
piayonl <piayonl@yahoo.com>
2020-09-23 06:27:49 -0400

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
